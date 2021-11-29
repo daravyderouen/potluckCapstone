f838b25d47b938f2cf5d83b416d97429fe68a92e
Daravy DeRouen <daravyderouen@gmail.com>
2021-11-29 12:23:49 -0600

    Adding files

---
+++ server/controller.js
@@ -0,0 +1,81 @@
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+const { CONNECTION_STRING } = process.env;
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
+
+
+module.exports = {
+    seed: (req, res) => {
+        sequelize.query(`
+
+
+        create table guests (
+            guest_id serial primary key, 
+            first_name varchar,
+            last_name varchar,
+            phone_number varchar,
+            dish_selected varchar
+        );
+
+        insert into guests (first_name, last_name, phone_number, dish_selected)
+        values ('MJ', 'DeRouen', '(888) 443-9090', 'Mac and Cheese'),
+            ('Eloise', 'Crabtree', '(777) 889-2323', 'Spaghetti'),
+            ('Ryker', 'Dupuy', '(111) 222-3333', 'Ham and Cheese sandwiches')
+            ;
+        `).then(() => {
+            console.log('DB seeded!')
+            res.sendStatus(200)
+        }).catch(err => console.log('error seeding DB', err))
+    },
+    getGuests: (req, res) => {
+        sequelize.query(`
+        SELECT * FROM guests;
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+
+    createGuest: (req, res) => {
+        let {
+            firstName,
+            lastName,
+            phoneNumber,
+            dishSelected,
+            
+        } = req.body;
+
+        sequelize
+            .query(
+                `insert into guests  
+        first_name = '${firstName}',
+        last_name = '${lastName}',
+        phone_number = '${phoneNumber}',
+        dish_selected = ${dishSelected}
+        `
+            )
+            .then(() => res.sendStatus(200))
+            .catch((err) => console.log(err));
+    },
+    deleteGuest: (req, res) => {
+        let {id} = req.params 
+        sequelize.query(`
+        DELETE
+            FROM guests
+            WHERE guest_id = ${id};
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    }
+}
+
+
---
+++ server/index.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const app = express()
+const {SERVER_PORT} = process.env
+const {seed, getGuests, createGuest} = require('./controller.js')
+
+
+app.use(express.json())
+app.use(cors())
+
+app.use(express.static("client"));
+app.get('/', (req, res) => {
+  res.sendFile('/index.html')
+
+})
+
+
+
+
+
+
+
+
+app.post('/seed', seed)
+
+app.get('/guests', getGuests)
+app.post('/guests', createGuest)
+
+/*
+app.get("/index.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.css"));
+    //rollbar.log('They need CSS!')
+  });
+  app.get("/index.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.js"));
+    //rollbar.log('Hey, they started me up again')
+  });
+*/
+
+
+
+
+
+
+//app.delete('/guests/:id', deleteGuest)
+
+const port = process.env.PORT || 4000
+
+//app.use(rollbar.errorHandler());
+
+
+
+
+
+
+
+app.listen(port, () => {  
+    console.log(`My server is on ${port}`)
+})
